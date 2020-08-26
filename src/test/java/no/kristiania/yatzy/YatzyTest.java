fc36f531c2f8cb8e19e15d5101b0d41a8e16af14
TrondEinar <trond-einar98@hotmail.no>
2020-08-26 15:28:57 +0200

    green test ONES, new test red

--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -12,7 +12,6 @@ public class YatzyTest {
     void shouldScoreOnes() {
 
         assertEquals(4, yatzyGame.score(YatzyCategory.ONES, new int [] {1, 2, 2, 1, 1}));
-        assertEquals(4, yatzyGame.score(YatzyCategory.ONES, new int [] {6, 5, 2, 1, 1}));
+        assertEquals(2, yatzyGame.score(YatzyCategory.ONES, new int [] {6, 5, 2, 1, 1}));
     }
-
 }
